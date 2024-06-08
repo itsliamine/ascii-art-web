e33c87336ef3f658888a7c66c7770e3345ae669f
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-08 16:51:23 +0200

    Added post and get handler

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	asciiart "ascii-art-web/ascii-art"
 	"ascii-art-web/core"
-	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"text/template"
 )
 
-func renderTemplate(w http.ResponseWriter, templ string, p *core.Page) {
-	t, _ := template.ParseFiles(templ + ".html")
-	t.Execute(w, p)
+type PageData struct {
+	Lines []string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	p, _ := core.LoadPage("test")
-	renderTemplate(w, "index", p)
+	w.WriteHeader(200)
+	t, _ := template.ParseFiles("templates/index.html")
+	data := PageData{}
+	f, _ := os.ReadFile("content.txt")
+	fileContent := string(f)
+	data.Lines = strings.Split(fileContent, "\n")
+	t.Execute(w, data)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	input := r.FormValue("input")
+	style := r.FormValue("banner")
+	output := asciiart.GetAscii(input, style)
+	core.Save(output)
+	http.Redirect(w, r, "/home", http.StatusFound)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/home", handler)
+	http.HandleFunc("/ascii", postHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
