4d78f5d84d7ae60b02f9469d3b68f96cfe76072b
Emixam26 <maxime.trebert@gmail.com>
2024-06-09 18:36:54 +0200

    fixed without mux

--- main.go
+++ main.go
@@ -7,19 +7,10 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlers.HomeHandler)
-	mux.HandleFunc("/ascii", handlers.PostHandler)
-	mux.HandleFunc("/404", handlers.NotFoundHandler)
-
-	// Custom 404 handler
-	mux.HandleFunc("/notfound", handlers.NotFoundHandler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/ascii", handlers.PostHandler)
+	http.HandleFunc("/404", handlers.NotFoundHandler)
 
 	log.Println("Server started at http://localhost:8080")
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
